ee517d919e4642fbadc1e06f0a1eb310521dc574
wujieli0207 <lwj0207@foxmail.com>
2022-02-22 07:27:43 +0800

    style: 路由增加 TS 类型

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,12 @@
-export const routes = [
+import { RouteRecordRaw } from "vue-router";
+
+export const routes: RouteRecordRaw[] = [
   {
     path: "/home",
     name: "home",
     component: () => import("/@/views/Home.vue"),
+    meta: {
+      name: "主页",
+    },
   },
 ];
