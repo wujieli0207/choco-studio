1cd3e51ba7c302868c9dc31734c52cc92ea10f1a
wujieli0207 <lwj0207@foxmail.com>
2022-03-04 20:22:40 +0800

    build: 引入 tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import "/@/styles/index.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import setupVant from "/@/plugins/vant";
-
 import { setupRouter } from "/@/router";
 
 function bootstrap() {
